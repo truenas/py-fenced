3adb4320c9f2111ab038f5936b4e3926850c82cb
caleb <yocalebo@gmail.com>
2020-09-25 11:42:26 -0400

    add version keyword to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='fenced',
+    version='0.0.1',
     description='TrueNAS SCALE Fencing Daemon',
     packages=find_packages(),
     classifiers=[
