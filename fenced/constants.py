5b81edf961203babd86e5541653dc7f6c9657539
caleb <yocalebo@gmail.com>
2025-08-25 11:47:38 -0400

    fix extremely slow start-up times

---
+++ fenced/constants.py
@@ -0,0 +1,5 @@
+# WARNING: If you change these, you _MUST_ change
+# the locations in the middleware code-base. These
+# are used in that repo as well.
+PID_FILE = "/var/run/middleware/fenced.pid"
+FENCED_ALERT_FILE = "/data/sentinels/.fenced-alert"
