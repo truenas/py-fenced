ded4b886bae1aa49ae20555847a7acbee9271062
caleb <yocalebo@gmail.com>
2025-08-25 12:00:23 -0400

    bump version
    (cherry picked from commit 1213cdf1afdc76e43fa6e08c588ffd7228db7490)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='fenced',
-    version='0.0.3',
+    version='0.0.4',
     description='TrueNAS SCALE Fencing Daemon',
     packages=find_packages(),
     classifiers=[
